a0771144808d62b69fde6aa9ae4cc4172dce57c0
Jovan Stankovic <zoxst@yahoo.com>
2018-08-05 11:18:09 +0200

    Initial Commit

---
+++ Test2018/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test2018
+//
+//  Created by Jovan Stankovic on 8/5/18.
+//  Copyright © 2018 Geoinformatika DOO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
